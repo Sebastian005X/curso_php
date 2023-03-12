85aa61f1ca385922466177181ebac47b5d99c036
Sebastian <kaynandredblue@gmail.com>
2023-03-12 14:26:20 -0500

    start POO

---
+++ 08-POO/00-poo.php
@@ -0,0 +1,30 @@
+<?php
+/* 
+La programación orientada a objetos es un paradigma de programación que se basa en la idea de
+que los programas se componen de objetos. 
+
+Un objeto es una instancia de una clase, que es una plantilla de la cual se crea el objeto y
+tambien define el comportamiento de sus propiedades y objetos.
+
+Un objeto esta compuesto por propiedades y metodos. Las propiedades son los atributos del objeto,
+y los metodos son las acciones que hace el objeto. Por ejemplo un objeto basado en un auto
+puede tener las siguientes propiedades: Color, marca y modelo y los siguientes metodos: 
+avanzar y retroceder.
+
+La POO se basa en cuatro principios fundamentales: la encapsulación, la herencia, el polimorfismo y la 
+abstracción.
+
+La encapsulación consiste en ocultar los detalles de implementación de un objeto y exponer solo una 
+interfaz pública para su uso. Esto ayuda a garantizar que los datos del objeto se manipulen de manera 
+consistente y segura.
+
+La herencia permite que una clase (la clase hija) herede atributos y métodos de otra clase 
+(la clase padre). Esto permite la reutilización de código y ayuda a organizar las clases en una 
+jerarquía.
+
+El polimorfismo permite que objetos de diferentes clases se traten de manera uniforme. Esto significa 
+que un objeto puede actuar de diferentes maneras según el contexto en el que se utilice.
+
+La abstracción se refiere a la capacidad de crear clases que representen conceptos abstractos. 
+Esto permite que los programadores trabajen a un nivel más alto de abstracción, lo que puede hacer que el código sea más fácil de entender y mantener.
+*/
